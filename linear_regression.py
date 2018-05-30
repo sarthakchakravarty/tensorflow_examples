2a732a46fd3eb4612fe7db74315c035639d854d3
sarthakchakravarty <sarthak.chakravarty@formcept.com>
2018-05-30 18:39:48 +0530

    :gift: Check loss function for a threshold value

--- linear_regression.py
+++ linear_regression.py
@@ -12,6 +12,22 @@ def create_random_data(n):
     return x, y
     
 
+def make_latest_loss(loss_list, loss):
+    if len(loss_list) == 5:
+        _ = loss_list.pop(0)
+        loss_list.append(loss)
+    else:
+        loss_list.append(loss)
+    return loss_list
+
+
+def check_loss_list(loss_list, threshold):
+    if abs(np.mean(np.diff(loss_list))) < threshold:
+        return True
+    else:
+        return False
+
+
 def linear_optimizer(n, lr, epoch):
     x_train, y_train = create_random_data(n)
     x_ = tf.placeholder(tf.float32)
@@ -27,23 +43,28 @@ def linear_optimizer(n, lr, epoch):
     
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
+        loss_list = []
         plt.ion()
         for i in range(epoch):
-            sess.run(train, feed_dict={x_: x_train, y_: y_train})
+            _, loss_val = sess.run([train, loss], feed_dict={x_: x_train, y_: y_train})
             weight = sess.run(w)
             bias = sess.run(b)
+            loss_list = make_latest_loss(loss_list, float(loss_val))
             # Plot best fit line on the points
             if not i % 20:
+                if check_loss_list(loss_list, 0.0001):
+                    break
+                else:
+                    pass
                 plt.cla()
                 plt.plot(x_train, y_train, 'rx')
                 plt.plot(x_train, weight * x_train + bias)
                 plt.show()
                 plt.pause(0.1)
         plt.ioff()
-        plt.show()
+        #plt.show()
         print("Weight: {0:.3f}, Bias: {1:.3f}, Loss: {2:.3f}"\
-              .format(float(weight), float(bias), 
-                      float(sess.run(loss, feed_dict={x_: x_train, y_: y_train}))))
+              .format(float(weight), float(bias), float(loss_val)))
                                                                          
                                                                          
 
